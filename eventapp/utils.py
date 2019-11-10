9902067020a52b4af370b9c99eeb3cb06512cc57
JBthePenguin <jblab976@gmail.com>
2019-11-10 18:51:04 +0100

    ok for calendar with events

--- eventapp/utils.py
+++ eventapp/utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import date
 from calendar import LocaleHTMLCalendar
 from .models import Event
 
@@ -9,33 +9,39 @@ class Calendar(LocaleHTMLCalendar):
         self.month = month
         super(Calendar, self).__init__()
 
-    # formats a day as a td
-    # filter events by day
-    def formatday(self, day, events):
+    def formatday(self, day, add_today, events):
+        # formats a day as a td
+        # text danger for today and background warning for event
         events_per_day = events.filter(start_time__day=day)
-        d = ''
-        for event in events_per_day:
-            d += f'<li> {event.title} </li>'
-
         if day != 0:
-            return f"<td><span class='date'>{day}</span><ul> {d} </ul></td>"
+            today = ""
+            event_day = ""
+            if len(events_per_day) != 0:
+                event_day = "bg-warning"
+            if add_today is not False:
+                if add_today.day == day:
+                    today = "text-danger"
+            return f"<td class='{event_day}'><span class='date {today}'>{day}</span></td>"
         return '<td></td>'
 
-    # formats a week as a tr
-    def formatweek(self, theweek, events):
+    def formatweek(self, theweek, add_today, events):
+        """ formats a week as a tr """
         week = ''
         for d, weekday in theweek:
-            week += self.formatday(d, events)
+            week += self.formatday(d, add_today, events)
         return f'<tr> {week} </tr>'
 
-    # formats a month as a table
-    # filter events by year and month
-    def formatmonth(self, withyear=True):
-        events = Event.objects.filter(
-            start_time__year=self.year, start_time__month=self.month)
+    def formatmonth(self, events, withyear=True):
+        """ formats a month as a table
+        with events for the month"""
         cal = f'<table border="0" cellpadding="0" cellspacing="0" class="calendar">\n'
         cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
         cal += f'{self.formatweekheader()}\n'
+        add_today = False
+        today = date.today()
+        if (today.month == self.month) and (today.year == self.year):
+            add_today = today
         for week in self.monthdays2calendar(self.year, self.month):
-            cal += f'{self.formatweek(week, events)}\n'
+            cal += f'{self.formatweek(week, add_today, events)}\n'
+        cal += f'</table>'
         return cal
--- eventapp/views.py
+++ eventapp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 import calendar
-from datetime import timedelta
-import datetime
+from datetime import timedelta, datetime
 from eventapp.utils import Calendar
 from songapp.views import MONTH
+from eventapp.models import Event
 
 
 def prev_month(d):
@@ -23,13 +23,17 @@ def next_month(d):
 
 def events(request, month):
     month_year = month.split('-')
-    day = datetime.datetime(int(month_year[0]), int(month_year[1]), 1)
+    day = datetime(int(month_year[0]), int(month_year[1]), 1)
+    events = Event.objects.filter(
+        start_time__year=day.year, start_time__month=day.month).order_by(
+        "start_time")
     event_calendar = Calendar(day.year, day.month)
     context = {
         'event_navbar': 'active',
-        'calendar': event_calendar.formatmonth(),
+        'calendar': event_calendar.formatmonth(events),
         'month_navbar': MONTH,
         'prev_month': prev_month(day),
         'next_month': next_month(day),
+        'events': events,
     }
-    return render(request, 'eventapp/calendar2.html', context)
+    return render(request, 'eventapp/calendar.html', context)
