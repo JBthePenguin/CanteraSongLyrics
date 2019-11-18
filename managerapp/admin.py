0d90a21ed79cae8db382be7ac8c693885ad02f88
JBthePenguin <jblab976@gmail.com>
2019-11-18 15:43:37 +0100

    ok for differents access to admin site for superuser  member and manager

--- managerapp/admin.py
+++ managerapp/admin.py
@@ -1,39 +1,38 @@
 from django.contrib import admin
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.admin import UserAdmin
 from managerapp.models import Manager, Member
 
 
-class ManagerAdminCreationForm(UserCreationForm):
-    pass
-
-    class Meta:
-        model = Manager
-        fields = UserCreationForm.Meta.fields + (
-            'first_name', 'last_name', 'email',)
-
-
 @admin.register(Manager)
-class ManagerAdmin(admin.ModelAdmin):
-    form = ManagerAdminCreationForm
+class ManagerAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': (
+            'username', 'password',
+            'first_name', 'last_name', 'email',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2')}
+         ),
+    )
     search_fields = ('username',)
     list_display = ('username', 'first_name', 'last_name', 'email')
 
-    def get_app_list(self, request):
-        """
-        Return a sorted list of all the installed apps that have been
-        registered in this site.
-        """
-        app_dict = self._build_app_dict(request)
-        # Sort the apps alphabetically.
-        app_list = sorted(app_dict.values(), key=lambda x: x['name'].lower())
-        # Sort the models alphabetically within each app.
-        for app in app_list:
-            app['models'].sort(key=lambda x: x['name'])
-        return app_list
 
-
+# MEMBER
 @admin.register(Member)
-class MemberAdmin(admin.ModelAdmin):
-    form = ManagerAdminCreationForm
+class MemberAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': (
+            'username', 'password',
+            'first_name', 'last_name', 'email',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2')}
+         ),
+    )
     search_fields = ('username',)
     list_display = ('username', 'first_name', 'last_name', 'email')
